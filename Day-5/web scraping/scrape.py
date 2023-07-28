32a2c461bee008a3832f9ab7730a314936dbe689
Anurag <anurag.chiplunkar@gmail.com>
2023-07-28 17:22:16 +0530

    notes

---
+++ Day-5/web scraping/scrape.py
@@ -0,0 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+response = requests.get("https://www.google.com/")
+if response.status_code != 200:
+	print("Error fetching page")
+	exit()
+else:
+    # Parsing the HTML with BeautifulSoup
+    soup = BeautifulSoup(response.content, 'html.parser')
+print(soup.title.string)
+nb_links = len(soup.find_all('a'))
+print(f"There are {nb_links} links in this page")
+
+# Targeting DOM elements
+first_link = soup.a
+print(first_link)
+print(first_link.text)
+print(first_link.get('href'))
+
